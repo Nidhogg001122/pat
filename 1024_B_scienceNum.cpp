4149fccd6f90e3cc8e78e560d43fbd1068a51c6f
nidhogg001122 <2208332100@qq.com>
2021-05-09 10:17:43 +0800

    [测试通过,但有问题]

--- 1024_B_scienceNum.cpp
+++ 1024_B_scienceNum.cpp
@@ -3,26 +3,26 @@
 #include<cstring>
 using namespace std;
 
-char num[10000] = { 'a' }, decimal[10000] = { 'a' };
+char num[10010] = { 'a' }, decimal[10010] = { 'a' }, E[10010];
 
 int main() {
 	cin >> num;
 	int i = 0, j = 0, k = 0, index = 0;
-	char integer = num[1], E[1000];
-	char c, judge=num[0];
+	char integer = num[1];
+	char c, judge = num[0];
 	for (i = 3; i < strlen(num); i++) {
 		if (num[i] != 'E') decimal[j++] = num[i];
 		else break;
 	}
 	for (i = i + 1; i < strlen(num); i++) E[k++] = num[i];
-	for (i = 1; i < k; i++) index = index * 10 + E[i]-48;
+	for (i = 1; i < k; i++) index = index * 10 + E[i] - 48;
 	if (E[0] == '+') {
 		num[0] = integer;
 		for (i = 1, k = 0; k < index; k++) {
-			if (decimal[k] != 'a') num[i++] = decimal[k];
+			if (k < j) num[i++] = decimal[k];
 			else num[i++] = '0';
 		}
-		if (decimal[k] != 'a') {
+		if (k < j) {
 			num[i++] = '.';
 			for (; k < j; k++) {
 				num[i++] = decimal[k];
@@ -32,14 +32,15 @@ int main() {
 	else {
 		i = 0;
 		if (index) {
-			num[0] = 0;
+			num[0] = '0';
 			num[1] = '.';
-			for (i = 2, k = 0; k < index; k++) num[i++] = '0';
+			for (i = 2, k = 1; k < index; k++) num[i++] = '0';
 		}
-
+		num[i++] = integer;
 		for (k = 0; k < j; k++) num[i++] = decimal[k];
 	}
 	if (judge == '-') printf("%c", judge);
 	for (k = 0; k < i; k++) printf("%c", num[k]);
+	printf("\n");
 	return 0;
 }
